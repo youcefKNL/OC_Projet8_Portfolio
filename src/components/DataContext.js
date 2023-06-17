48fe38fa57012520e993138b324dff8d5ec56aab
youcefKNL <khenichil_youcef@hotmail.fr>
2023-06-17 19:10:12 +0200

    gallery v3

--- src/components/DataContext.js
+++ src/components/DataContext.js
@@ -25,6 +25,34 @@
 //     <DataContext.Provider value={{ data }}>{children}</DataContext.Provider>
 //   );
 // };
+// import React, { createContext, useState, useEffect } from "react";
+// import axios from "axios";
+
+// export const DataContext = createContext();
+
+// export const DataDone = ({ children }) => {
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       // Mettre l'adresse de la future API dans la const url (https...)
+//       const url = "../../data/data.json";
+//       try {
+//         const response = await axios.get(url);
+//         const jsonData = response.data;
+//         setData(jsonData);
+//       } catch (error) {
+//         console.log("Problème de communication avec l'API");
+//         console.log(error);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <DataContext.Provider value={{ data }}>{children}</DataContext.Provider>
+//   );
+// };
 import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
@@ -32,6 +60,7 @@ export const DataContext = createContext();
 
 export const DataDone = ({ children }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); // Ajout du state pour le loader
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,13 +70,20 @@ export const DataDone = ({ children }) => {
         const response = await axios.get(url);
         const jsonData = response.data;
         setData(jsonData);
+        setLoading(false); // Mettre le loader à false une fois les données chargées
       } catch (error) {
         console.log("Problème de communication avec l'API");
         console.log(error);
+        setLoading(false); // Mettre le loader à false en cas d'erreur de communication
       }
     };
     fetchData();
   }, []);
+
+  // Afficher le loader tant que les données sont en cours de chargement
+  if (loading) {
+    return <div>Chargement en cours...</div>;
+  }
 
   return (
     <DataContext.Provider value={{ data }}>{children}</DataContext.Provider>
--- src/pages/Galery.js
+++ src/pages/Galery.js
@@ -1,10 +1,45 @@
-// Galery.js
-import React, { useContext } from "react";
+// // Galery.js
+// import React, { useContext } from "react";
+// import { DataContext } from "../components/DataContext";
+// import Card from "../components/Card";
+
+// const Galery = () => {
+//   const { data } = useContext(DataContext);
+
+//   return (
+//     <section className="galery">
+//       <h1>Mes réalisations & Projets finis</h1>
+//       {data.map((data) => {
+//         return <Card data={data} key={data.id} />;
+//       })}
+//     </section>
+//   );
+// };
+
+// export default Galery;
+
+import React, { useContext, useState, useEffect } from "react";
 import { DataContext } from "../components/DataContext";
 import Card from "../components/Card";
+import { TailSpin } from "react-loader-spinner";
 
 const Galery = () => {
   const { data } = useContext(DataContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      setIsLoading(false);
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <div className="spinner-container">
+        <TailSpin color="#000" height={80} width={80} />
+      </div>
+    );
+  }
 
   return (
     <section className="galery">
